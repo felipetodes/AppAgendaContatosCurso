20b0647f18e873330d16133174d2e2d39c17c208
Felipe Todeschini <todeschini1992@gmail.com>
2025-01-04 20:43:49 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace AppAgendaContatosCurso
+{
+    public class Program
+    {
+        public static readonly string connectionString = "Server=Localhost;Database=AGENDA;Integrated Security=True;";
+        static void Main(string[] args)
+        {
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                try { 
+                
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Erro ao estabelecer conexão com o banco de dados");
+                }
+                finally 
+                { 
+                    con.Close();
+                }
+            }
+        }
+    }
+}
