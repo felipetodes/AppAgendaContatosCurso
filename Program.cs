4d074195bfe381e1acc2128da58cc2bd9670be5d
Felipe Todeschini <todeschini1992@gmail.com>
2025-01-04 21:37:45 -0300

    AppAgendaContatosCuwhile (reader.Read())rso

--- Program.cs
+++ Program.cs
@@ -7,18 +7,44 @@ namespace AppAgendaContatosCurso
         public static readonly string connectionString = "Server=Localhost;Database=AGENDA;Integrated Security=True;";
         static void Main(string[] args)
         {
-            using(SqlConnection con = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                try { 
+                try {
+                    connection.Open();
+
+                    string query = "SELECT ID, NOME, EMAIL, DT_INC FROM CONTATO";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Forma 1 
+                                //Console.WriteLine(reader.GetInt32(0));
+                                //Console.WriteLine(reader.GetString(1));
+                                //Console.WriteLine(reader.GetString(2));
+                                //Console.WriteLine(reader.GetDateTime(3));
+
+                                // Forma 2
+                                Console.WriteLine(reader["ID"]);
+                                Console.WriteLine(reader["NOME"]);
+                                Console.WriteLine(reader["EMAIL"]);
+                                Console.WriteLine(reader["DT_INC"]);
+
+                            }
+                        }
+                    }
+
                 
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
-                    Console.WriteLine("Erro ao estabelecer conexão com o banco de dados");
+                    Console.WriteLine($"Erro ao estabelecer conexão com o banco de dados.{ex.Message}");
                 }
                 finally 
-                { 
-                    con.Close();
+                {
+                    connection.Close();
                 }
             }
         }
