241a3d33b6cff307faedf8d88eb362fca04c901d
Felipe Todeschini <todeschini1992@gmail.com>
2025-01-04 21:47:49 -0300

    public static void ExecutarSelect()

--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ namespace AppAgendaContatosCurso
         public static readonly string connectionString = "Server=Localhost;Database=AGENDA;Integrated Security=True;";
         static void Main(string[] args)
         {
+            ExecutarSelect();
+        }
+        public static void ExecutarSelect()
+        {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                try {
+                try
+                {
                     connection.Open();
 
                     string query = "SELECT ID, NOME, EMAIL, DT_INC FROM CONTATO";
@@ -27,22 +32,22 @@ namespace AppAgendaContatosCurso
                                 //Console.WriteLine(reader.GetDateTime(3));
 
                                 // Forma 2
-                                Console.WriteLine(reader["ID"]);
-                                Console.WriteLine(reader["NOME"]);
-                                Console.WriteLine(reader["EMAIL"]);
-                                Console.WriteLine(reader["DT_INC"]);
+                                Console.WriteLine($"ID: {reader["ID"]}");
+                                Console.WriteLine($"NOME: {reader["NOME"]}");
+                                Console.WriteLine($"EMAIL: {reader["EMAIL"]}");
+                                Console.WriteLine($"DATA DE CRIAÇÃO: {reader["DT_INC"]}");
 
                             }
                         }
                     }
 
-                
+
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Erro ao estabelecer conexão com o banco de dados.{ex.Message}");
                 }
-                finally 
+                finally
                 {
                     connection.Close();
                 }
